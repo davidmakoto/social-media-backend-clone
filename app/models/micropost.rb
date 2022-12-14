f5eef6a7f9dc0d55980ba194a7fcaf418504947d
David <davidmacotoward@gmail.com>
2022-12-14 13:09:24 -0800

    fixed typo

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,5 @@
 class Micropost < ApplicationRecord
     belongs_to :user
     validates :content, length: { maximum: 140 }, presence: true
-    validates, :user_id, presence: true
+    validates :user_id, presence: true
 end
