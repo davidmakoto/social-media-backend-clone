ffc53f849055f3359db5276b409ba265abc1c5f6
David <davidmacotoward@gmail.com>
2022-12-14 15:09:14 -0800

    basic page controller and micropost validations

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/help'
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
