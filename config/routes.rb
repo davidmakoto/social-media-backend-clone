f7225c6acf4e2776da0e0ea1c2931ece07c96f1d
dmw <davidmacotoward@gmail.com>
2023-01-17 12:02:24 -0800

    added authentication using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
+
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
 
+  devise_for :users
+
   resources :microposts
   resources :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "microposts#index"
-  # root "users#index"
 end
