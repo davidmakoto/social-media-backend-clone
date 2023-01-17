f16893f8954c8c65538bd5c9a8c758032f8cf4a7
dmw <davidmacotoward@gmail.com>
2023-01-17 12:02:45 -0800

    added button styles and few UI tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,18 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
     has_many :microposts
 
-    validates :name,
-    allow_blank: false,
-    uniqueness: { case_sensitive: false },
-    length: { minimum: 3, maximum: 16 },
-    format: {
-      with: /\A[a-zA-Z0-9_]+\Z/i,
-      message: "can only contain letters, numbers, periods or underscores"
-    }
+    # validates :name,
+    # allow_blank: false,
+    # uniqueness: { case_sensitive: false },
+    # length: { minimum: 3, maximum: 16 },
+    # format: {
+    #   with: /\A[a-zA-Z0-9_]+\Z/i,
+    #   message: "can only contain letters, numbers, periods or underscores"
+    # }
     validates_presence_of :email
 end
