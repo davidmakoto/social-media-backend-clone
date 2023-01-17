ab91961a53d0311d1cdfd4082141027ff38bd580
dmw <davidmacotoward@gmail.com>
2023-01-16 17:13:19 -0800

    nav bar and wip styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
+  get 'static_pages/contact'
+
   resources :microposts
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
