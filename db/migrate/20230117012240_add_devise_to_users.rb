d1616ba50f3e9a90f19c00781ef55b0ecc095ee0
dmw <davidmacotoward@gmail.com>
2023-01-18 21:27:46 -0800

    added username to users

--- db/migrate/20230117012240_add_devise_to_users.rb
+++ db/migrate/20230117012240_add_devise_to_users.rb
@@ -5,6 +5,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
     change_table :users do |t|
       ## Database authenticatable
       t.string :encrypted_password, null: false, default: ""
+      t.string :username, null: false, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
