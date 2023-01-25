a6414da036542a8dd64e9b8f325cb879c80fde26
dmw <davidmacotoward@gmail.com>
2023-01-25 10:15:27 -0800

    removed status pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-
   devise_for :users
 
   resources :microposts
