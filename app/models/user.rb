d61301f499f7917d29c0c69cb7ffffba213649cb
dmw <davidmacotoward@gmail.com>
2023-01-25 10:36:21 -0800

    small changes to user model and form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    has_many :microposts
+  has_many :microposts
 
-    # validates :name,
-    # allow_blank: false,
-    # uniqueness: { case_sensitive: false },
-    # length: { minimum: 3, maximum: 16 },
-    # format: {
-    #   with: /\A[a-zA-Z0-9_]+\Z/i,
-    #   message: "can only contain letters, numbers, periods or underscores"
-    # }
-    validates_presence_of :email
+  validates :name,
+  allow_blank: false,
+  uniqueness: { case_sensitive: false },
+  length: { minimum: 3, maximum: 16 },
+  format: {
+    with: /\A[a-zA-Z0-9_]+\Z/i,
+    message: "can only contain letters, numbers, periods or underscores"
+  }
+  validates_presence_of :email
 end
